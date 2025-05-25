766451b420a0f3519ebf6077c642f30a169a4aa2
Karl Wette <karl.wette@anu.edu.au>
2025-05-25 21:35:57 +1000

    Version 1.1

--- src/py_api_dumper/__init__.py
+++ src/py_api_dumper/__init__.py
@@ -10,7 +10,7 @@ from types import ModuleType
 from typing import List, Optional, TextIO, Tuple, Type, TypeVar, Union
 
 __author__ = "Karl Wette"
-__version__ = "1.0"
+__version__ = "1.1"
 
 APIDumpType = TypeVar("APIDumpType", bound="APIDump")
 
